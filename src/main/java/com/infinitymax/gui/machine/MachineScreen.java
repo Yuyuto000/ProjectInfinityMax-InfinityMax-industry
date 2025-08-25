3ea82bdaebf434579907cc6d1a91deb85ee9ef1c
Yuyuto <yukkurigame0101@gmail.com>
2025-08-25 13:53:33 +0900

    MachineScreen.java を更新

--- src/main/java/com/infinitymax/gui/machine/MachineScreen.java
+++ src/main/java/com/infinitymax/gui/machine/MachineScreen.java
@@ -7,6 +7,12 @@ import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.client.gui.Font;
 
+/**
+ * Machine の GUI スクリーン
+ * - プログレスバー
+ * - エネルギーバー
+ * - 流体タンクバー (2 本)
+ */
 public class MachineScreen extends AbstractContainerScreen<MachineMenu> {
 
     private static final ResourceLocation BG = new ResourceLocation("infinitymax_industry", "textures/gui/machine_bg.png");
@@ -18,20 +24,30 @@ public class MachineScreen extends AbstractContainerScreen<MachineMenu> {
     }
 
     @Override
-    protected void renderBg(PoseStack pPoseStack, float pPartialTick, int pMouseX, int pMouseY) {
+    protected void renderBg(PoseStack ps, float partialTicks, int mouseX, int mouseY) {
         this.getMinecraft().getTextureManager().bind(BG);
-        blit(pPoseStack, leftPos, topPos, 0, 0, imageWidth, imageHeight, imageWidth, imageHeight);
+        blit(ps, leftPos, topPos, 0, 0, imageWidth, imageHeight, imageWidth, imageHeight);
 
-        // draw progress bar reading data[0] (progress) and data[1] (progressMax)
+        // プログレスバー
         int prog = menu.data.get(0);
         int progMax = menu.data.get(1);
         int px = progMax == 0 ? 0 : (int) ((double) prog / progMax * 24.0);
-        blit(pPoseStack, leftPos + 79, topPos + 34, 176, 0, px, 16, 256, 256);
+        blit(ps, leftPos + 79, topPos + 34, 176, 0, px, 16, 256, 256);
 
-        // energy bar (data[2] currentJ, data[3] capacity)
+        // エネルギーバー
         int cur = menu.data.get(2), cap = menu.data.get(3);
         int ey = cap == 0 ? 0 : (int) ((double) cur / cap * 50.0);
-        blit(pPoseStack, leftPos + 152, topPos + 16 + (50 - ey), 176, 16 + (50 - ey), 6, ey, 256, 256);
+        blit(ps, leftPos + 152, topPos + 16 + (50 - ey), 176, 16 + (50 - ey), 6, ey, 256, 256);
+
+        // 流体タンク 0
+        int f0 = menu.data.get(4);
+        int fy0 = (int)((double)f0 / 16000.0 * 50.0);
+        blit(ps, leftPos + 162, topPos + 16 + (50 - fy0), 182, 16 + (50 - fy0), 6, fy0, 256, 256);
+
+        // 流体タンク 1
+        int f1 = menu.data.get(5);
+        int fy1 = (int)((double)f1 / 16000.0 * 50.0);
+        blit(ps, leftPos + 170, topPos + 16 + (50 - fy1), 188, 16 + (50 - fy1), 6, fy1, 256, 256);
     }
 
     @Override
@@ -42,8 +58,8 @@ public class MachineScreen extends AbstractContainerScreen<MachineMenu> {
     }
 
     @Override
-    protected void renderLabels(PoseStack pPoseStack, int pMouseX, int pMouseY) {
+    protected void renderLabels(PoseStack ps, int mouseX, int mouseY) {
         Font font = this.font;
-        font.draw(pPoseStack, title, 8.0F, 6.0F, 4210752);
+        font.draw(ps, title, 8.0F, 6.0F, 4210752);
     }
 }
